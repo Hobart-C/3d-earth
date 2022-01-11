63e87b177ff842d396c271d53cdffc636d48ce5b
Hobart <yiliang.chen@einyun.com>
2022-01-11 17:13:45 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <router-view style="height: 100%" />
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+* {
+  box-sizing: border-box;
+}
+
+html,
+body,
+#app,
+#home {
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
+</style>
